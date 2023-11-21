46ebbc81efd454d5a81ff0017d2abb803cd60d63
HalfordOsbourne98 <101620775+HalfordOsbourne98@users.noreply.github.com>
2023-11-21 08:35:37 +0100

    Update MyFileStor.sol

--- MyFileStor.sol
+++ MyFileStor.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract DecentralizedFileStorage {
+    mapping(address => string[]) public userFiles;
+
+    event FileUploaded(address indexed uploader, string indexed fileName, string fileContent);
+
+    modifier validFileName(string memory _fileName) {
+        require(bytes(_fileName).length > 0, "File name cannot be empty");
+        _;
+    }
+
+  
+}
