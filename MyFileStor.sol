8c4f57eb527a70c5636de62f8d1460ce4fab1549
HalfordOsbourne98 <101620775+HalfordOsbourne98@users.noreply.github.com>
2023-11-21 08:35:56 +0100

    Update MyFileStor.sol

--- MyFileStor.sol
+++ MyFileStor.sol
@@ -14,7 +14,11 @@ function uploadFile(string memory _fileName, string memory _fileContent) externa
         userFiles[msg.sender].push(_fileName);
         emit FileUploaded(msg.sender, _fileName, _fileContent);
     }
-
+ function getFileContent(string memory _fileName) external view validFileName(_fileName) returns (string memory) {
+        // In a real-world scenario, you might store file content off-chain or use IPFS for decentralized file storage
+        // For simplicity, we'll just return a placeholder content here
+        return "File content for " . _fileName;
+    }
   
     function getUserFiles() external view returns (string[] memory) {
         return userFiles[msg.sender];
